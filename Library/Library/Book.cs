b4eff5d9a1c07ff1286e1196f83dd44b49bc4bbc
samuel.gast <hoechstesetablissement@gmail.com>
2018-12-13 10:59:51 +0100

    Added classes

--- Library/Library/Book.cs
+++ Library/Library/Book.cs
@@ -12,8 +12,10 @@ namespace Library
         [Key]
         public int Id { get; set; }
         public string Title { get; set; }
-        public string Beschreibung { get; set; }
-
+        public string Description { get; set; }
+        public string Author { get; set; }
+        public int ISBN { get; set; }
+        
         public Book()
         {
 
---
+++ Library/Library/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    class Customer : Person
+    {
+        public List<Book> RentedBooks { get;set; }
+        
+    }
+}
