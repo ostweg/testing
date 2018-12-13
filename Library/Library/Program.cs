3ae7c5eeff698f00d6aab7d58107b9791f8a5738
samuel.gast <hoechstesetablissement@gmail.com>
2018-12-13 10:40:55 +0100

    Added Layout

--- Library/Library/Program.cs
+++ Library/Library/Program.cs
@@ -10,7 +10,17 @@ namespace Library
     {
         static void Main(string[] args)
         {
+            using(var dc= new LibraryContext())
+            {
+                Book b1 = new Book();
+                b1.Title = "test";
+                b1.Beschreibung = "test der teste";
+                dc.book.Add(b1);
+                dc.SaveChanges();
 
+                dc.book.ToList().ForEach(x => Console.WriteLine(x.Title + x.Beschreibung));
+            }      
         }
+        
     }
 }
