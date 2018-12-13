6a73b143a5a24e504dca63da7126aa1eeb0642d3
philipmetzgercsbe <philip.metzger@csbe.ch>
2018-12-13 11:32:16 +0100

    Added Rental

---
+++ Library/Library/Rental.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    class Rental
+    {
+        public Book Book { get; set; }
+        public Customer Customers { get; set; }
+        public Employee Employee { get; set; }
+
+        public Rental(Book book, Customer customers, Employee employee)
+        {
+            Book = book;
+            Customers = customers;
+            Employee = employee;
+        }
+    }
+}
